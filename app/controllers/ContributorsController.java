ce8605ef1aba732ceb11208a857a8dc6842b77c1
Kerven Albuquerque <kerven.albuquerque@gmail.com>
2021-03-24 00:50:28 -0300

    Get dynamic data and add authorization

--- app/controllers/ContributorsController.java
+++ app/controllers/ContributorsController.java
@@ -21,57 +21,70 @@ import java.util.Comparator;
 import java.util.Iterator;
 import java.time.temporal.ChronoUnit;
 import java.time.Duration;
+import com.typesafe.config.Config;
 
 public class ContributorsController extends Controller {
     private final HttpExecutionContext ec;
     private final WSClient ws;
+    private final Config config;
 
     @Inject
-    public ContributorsController(HttpExecutionContext ec, WSClient ws) {
+    public ContributorsController(HttpExecutionContext ec, WSClient ws, Config config) {
         this.ec = ec;
         this.ws = ws;
+        this.config = config;
     }
 
     public CompletionStage<Result> index(Http.Request request, String organization) {
-        return ws.url("https://api.github.com/orgs/github/repos").get().thenApplyAsync(response -> {
-            JsonNode jsonRepos = response.getBody(WSBodyReadables.instance.json());
-            Map<String, Integer> contributions = new HashMap<String, Integer>();
+        return ws.url("https://api.github.com/orgs/" + organization + "/repos")
+                .addHeader("Accept", "application/vnd.github.v3+json")
+                .addHeader("Authorization", "token " + config.getString("gh_token")).get().thenApplyAsync(response -> {
+                    if (response.getStatus() != 200) {
+                        ObjectNode responseBody = response.asJson().deepCopy();
+                        return status(response.getStatus(), responseBody.retain("message"));
+                    }
+                    JsonNode jsonRepos = response.getBody(WSBodyReadables.instance.json());
+                    Map<String, Integer> contributions = new HashMap<String, Integer>();
 
-            CompletableFuture[] repos = new CompletableFuture[jsonRepos.size()];
-            for (int i = 0; i < jsonRepos.size(); i++)
-                repos[i] = ws.url(jsonRepos.get(i).get("contributors_url").asText())
-                        .setRequestTimeout(Duration.of(1000, ChronoUnit.MILLIS)).get().thenApplyAsync(repoContrib -> {
-                            JsonNode jsonContrib = repoContrib.getBody(WSBodyReadables.instance.json());
-                            jsonContrib.forEach(userContrib -> {
-                                String name = userContrib.get("login").asText();
-                                Integer contrib = userContrib.get("contributions").asInt()
-                                        + contributions.getOrDefault(name, 0);
-                                contributions.put(name, contrib);
-                            });
-                            return null;
-                        }, ec.current()).exceptionally(result -> null).toCompletableFuture();
+                    CompletableFuture[] repos = new CompletableFuture[jsonRepos.size()];
+                    for (int i = 0; i < jsonRepos.size(); i++)
+                        repos[i] = ws.url(jsonRepos.get(i).get("contributors_url").asText())
+                                .addHeader("Accept", "application/vnd.github.v3+json")
+                                .addHeader("Authorization", "token " + config.getString("gh_token"))
+                                .setRequestTimeout(Duration.of(1000, ChronoUnit.MILLIS)).get()
+                                .thenApplyAsync(repoContrib -> {
+                                    JsonNode jsonContrib = repoContrib.getBody(WSBodyReadables.instance.json());
+                                    jsonContrib.forEach(userContrib -> {
+                                        String name = userContrib.get("login").asText();
+                                        Integer contrib = userContrib.get("contributions").asInt()
+                                                + contributions.getOrDefault(name, 0);
+                                        contributions.put(name, contrib);
+                                    });
+                                    return null;
+                                }, ec.current()).exceptionally(result -> null).toCompletableFuture();
 
-            CompletableFuture.allOf(repos).join();
+                    CompletableFuture.allOf(repos).join();
 
-            List<ObjectNode> contributors = new ArrayList<ObjectNode>();
-            contributions.forEach((key, value) -> {
-                ObjectNode objectNode = JsonNodeFactory.instance.objectNode();
-                objectNode.put("name", key);
-                objectNode.put("contributions", value);
-                contributors.add(objectNode);
-            });
-            Collections.sort(contributors, new Comparator<ObjectNode>() {
-                @Override
-                public int compare(ObjectNode o1, ObjectNode o2) {
-                    return o2.get("contributions").asInt() - o1.get("contributions").asInt();
-                }
-            });
-            return ok(Json.toJson(contributors));
+                    List<ObjectNode> contributors = new ArrayList<ObjectNode>();
+                    contributions.forEach((key, value) -> {
+                        ObjectNode objectNode = JsonNodeFactory.instance.objectNode();
+                        objectNode.put("name", key);
+                        objectNode.put("contributions", value);
+                        contributors.add(objectNode);
+                    });
+                    Collections.sort(contributors, new Comparator<ObjectNode>() {
+                        @Override
+                        public int compare(ObjectNode o1, ObjectNode o2) {
+                            return o2.get("contributions").asInt() - o1.get("contributions").asInt();
+                        }
+                    });
+                    return ok(Json.toJson(contributors));
 
-        }, ec.current()).exceptionally(result -> {
-            ObjectNode objectNode = JsonNodeFactory.instance.objectNode();
-            objectNode.put("message", "API limit exceeded");
-            return forbidden(Json.toJson(objectNode));
-        });
+                }, ec.current()).exceptionally(result -> {
+                    System.out.println(result);
+                    ObjectNode objectNode = JsonNodeFactory.instance.objectNode();
+                    objectNode.put("message", "API limit exceeded");
+                    return forbidden(Json.toJson(objectNode));
+                });
     }
 }
